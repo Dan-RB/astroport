cb9654a29f668cf43a50247aa614436a579b169a
Dan-RB <102555523+Dan-RB@users.noreply.github.com>
2024-04-20 19:43:29 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://Dan-RB.github.io',
+  site: 'https://Dan-RB.github.io'
 });
